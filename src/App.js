f9150706cf41f50437ce3ed45d3395bc96b11850
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-09-04 21:35:53 -0300

    exercice done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg'
 import './App.css'
 import React from 'react'
-import Clock2 from './Clock2'
 import Clock from './Clock'
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<Clock2></Clock2>
 				<Clock></Clock>
 			</header>
 			<body>
--- src/Clock.js
+++ src/Clock.js
@@ -1,45 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Clock = () => {
-	const state = {
+	const [state, setUser] = useState({
 		date: new Date(),
 		age: 0,
 		name: 'Example Name',
 		surname: 'Example Surname'
-	}
-	const tick = () => {
-		this.setState((prevState) => {
-			let edad = prevState.edad +1
-			return {
-				...prevState,
-				fecha: new Date(),
-				edad
-			}
+	})
+	useEffect(() => {
+		let interval = null
+		interval = setInterval(() => {
+			actualiceUser()
+		}, 1000)
+		return () => clearInterval(interval)
+	}, [state])
+	
+	const actualiceUser = () => {
+		return setUser({
+			date: new Date(),
+			age: state.age + 1,
+			name: state.name,
+			surname: state.surname,
 		})
 	}
-	//componentDidMount
-	useEffect(() => {
-		this.timerID = setInterval (
-			() => this.tick(), 1000
-		)
-	}, [])
-	useEffect(() => {
-		// componentWillUnmount
-		return () => {
-			clearInterval(this.timerID)
-		}
-	}, [tick])
-
-	
 
 	return (
 		<div>
 			<h2>
             Hora Actual:
-				{state.fecha.toLocaleTimeString()}
+				{state.date.toLocaleTimeString()}
 			</h2>
-			<h3>{state.nombre} {state.apellidos}</h3>
-			<h1>Edad: {state.edad}</h1>
+			<h3>{state.name} {state.surname}</h3>
+			<h1>Edad: {state.age}</h1>
 		</div>
 	)
 }
